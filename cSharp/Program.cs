91f18263d8801a442309552dcc7a20b4ef15db80
Seconds <khameye19888@gmail.com>
2024-01-27 18:28:55 -0600

    Add project files.

---
+++ cSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello Onedur");
+        }
+    }
+}
+
+// This is not a line of code but a comment
+
+/* This is for a 
+
+block of comments. */
