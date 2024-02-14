d52cac168624f5fd6b55f277650d913bcacbc06c
Khameye Khamhuang <khameye19888@gmail.com>
2024-02-14 15:12:05 -0600

    upload

---
+++ cSharp/Stop/Stop/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Making of a visual "Stop" sign
+
+Console.WriteLine("   XXXXXX ");
+Console.WriteLine(" X        X ");
+Console.WriteLine("X   STOP   X");
+Console.WriteLine(" X        X ");
+Console.WriteLine("   XXXXXX ");
+Console.WriteLine("      x      ");
+Console.WriteLine("      x      ");
+Console.WriteLine("      x      ");
